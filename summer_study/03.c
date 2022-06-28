d6543c1a5520515b6079c6919228bc8a70d80abc
JunSeok <ppu1234@naver.com>
2022-06-28 18:05:56 +0900

    0628_commit

--- summer_study/03.c
+++ summer_study/03.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 int main() {
-	int x = 80; // 1001011, 1011111, 1001010, 1000111
+	int x = 75;
 
-	int mask;
-	mask = 1 << 6; // 1000000
-    // 0100000
+	int mask1 = 1 << 6; // 1000000
+	int mask2 = mask1 >> 1; // 0100000
+	int mask3 = mask1 >> 2; // 0010000
+	int mask4 = mask1 >> 3; // 0001000
+	int mask5 = mask1 >> 4; // 0000100
+	int maks6 = mask1 >> 5; // 0000010
+	int mask7 = mask1 >> 6; // 0000001
 
-    // 0010000, 0001000, 0000100
+	int maskNum1 = mask1 >> 1;
+	int maskNum2 = mask3 | mask4 | mask5; // 0011100 // 1로 지정
+	int maskNum3 = mask5 ^ mask7; // 0000101
+	int maskNum33 = ~maskNum3; // 1111010
+	int maskNum4 = x ^ mask4; // 1과 ^연산
+	int maskNum44 = maskNum4 ^ mask5; // 1과 ^연산
 
-    // 0000100 0000001
-
-	printf("1번 답%d\n", x & mask >> 1);
-	printf("2번 답%d\n", x | mask >> 2 & x | mask >> 3 & x | mask >> 4);
-	printf("3번 답%d\n", x ^ mask >> 4, x ^ mask >> 6);
-	printf("4번 답%d\n", x ^ mask >> 3, x ^ mask >> 4);
+	printf("%d\n", x & maskNum1);
+	printf("%d\n", x | maskNum2); // 1로 지정 // 1과 |연산
+	printf("%d\n", x & maskNum33); // 0으로 지정 / 0과 &연산
+	printf("%d\n", maskNum44); // 반전 / 1과 ^연산
 
 	return 0;
 }
+// 0으로 지정할 때 0과 &연산
+// 1로 지정할 때 1과 |연산
+// 반전시킬 때 1과 ^연산
