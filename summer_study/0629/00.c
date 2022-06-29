4c2eb215ff547ac6fbc2f6b04c620ef2c64111eb
JunSeok <ppu1234@naver.com>
2022-06-29 20:46:53 +0900

    0629_commit

---
+++ summer_study/0629/00.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int i, j, a, b;
+
+	scanf("%d", &a); // 첫 번째 입력받은 수
+	int arr[10000];
+	arr[0] = a;
+
+	if (a != 0) {
+		for (i = 0;; i++) {
+			scanf("%d", &b);
+			if (b == 0) break;
+			arr[i + 1] = b;
+		}
+		printf("%d\n", i+1);
+		for (j = i; j >= 0; j--) {
+			printf("%d ", arr[j]);
+		}
+	}
+	else {
+		printf("%d\n%d", 0, arr[0]);
+	}
+	return 0;
+}
