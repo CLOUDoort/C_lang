9bfca5c2412b36dc322d047dfed35e8d5f3b6b27
JunSeok <ppu1234@naver.com>
2022-07-02 01:24:58 +0900

    0702_commit

--- summer_study/0701_Midterm/01.c
+++ summer_study/0701_Midterm/01.c
@@ -2,17 +2,26 @@
 #include <stdio.h>
 
 int main() {
-    int C, index = 0, guess;
+    int C, index = 1, guess;
     double F;
     scanf("%d", &C);
-    F = 9.0/5.0 * C + 32;
+    F = (int)9.0/5.0 * C + 32;
+
+    int fInt = F + 0.5;
 
     while(1) {
         scanf("%d", &guess);
-        if(guess < F) {
-            printf("small");
+        if(guess < fInt) {
+            printf("small\n");
             index++;
+        } else if(guess > fInt) {
+            printf("large\n");
+            index++;
+        } else {
+            printf("correct %d\n", index);
+            break;
         }
-        break;
     }
+
+    return 0;
 }
